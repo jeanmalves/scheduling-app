1bb1b790053c2a0292c95b4e282c7d3fff69ffbb
Jeam Alves <jeanmalves@gmail.com>
2021-09-22 23:49:20 -0400

    Added footer to layout

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,19 @@
     <NavBar />
     <Banner />
     <router-view />
+    <Footer />
   </header>
 </template>
 <script>
 import NavBar from '@/components/layout/NavBar.vue';
 import Banner from '@/components/Banner.vue';
+import Footer from '@/components/layout/Footer.vue';
 
 export default {
   components: {
     NavBar,
     Banner,
+    Footer,
   },
 };
 </script>
