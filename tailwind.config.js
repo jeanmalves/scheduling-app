e5138f2ee49bac7d9a74a7ff157d70f614fbd03d
Jeam Alves <jeanmalves@gmail.com>
2021-09-25 00:02:50 -0400

    Installed for ratio in card service list

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const aspectRatio = require('@tailwindcss/aspect-ratio');
+
 module.exports = {
   purge: ['index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
@@ -7,5 +9,7 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    aspectRatio,
+  ],
 };
