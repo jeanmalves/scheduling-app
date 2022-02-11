1e328ac3190e1fbb91c9030acb9b5487a5ab51c8
Jeam Alves <jeanmalves@gmail.com>
2022-02-10 22:38:00 -0400

    Added route for schedule services

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import Schedule from '../views/Schedule.vue';
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,12 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/schedule/services/:id',
+    name: 'schedule',
+    component: Schedule,
+    props: true,
   },
 ];
 
